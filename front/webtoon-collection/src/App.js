2f7a215298ff4061f614822e794a7e6a3a442992
JettiJAM <jettijam@gmail.com>
2021-05-19 21:11:09 +0900

    [#5] Initial commit

---
+++ front/webtoon-collection/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> 다람쥐 도토리 개많이 쳐먹어</h1>
+      <p> place holding lorem ipsum shit </p>
+    </div>
+  );
+}
+
+export default App;
